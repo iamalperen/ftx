31d92d73fe59e30d640bd490ce7a272bcf7ffbb9
Alperen Talaslıoğlu <alperen.talaslioglu@gmail.com>
2022-01-27 09:58:33 +0300

    Styled Components Added, Normalize.css Added

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Normalize } from 'styled-normalize'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Normalize />
     <div>FTX NFT</div>
   </React.StrictMode>,
   document.getElementById('root')
